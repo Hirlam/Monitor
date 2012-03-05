dbf7e246166f39a8ebee1fd0aae6a8ed9a63722d
uandrae <uandrae@9085bc96-f90c-0410-a38d-ac8cfabbdae3>
2012-03-05 11:53:23 +0000

    Ulf Andrae: Partly revert [10392]
    git-svn-id: https://svn.hirlam.org/trunk/harmonie/util/monitor@10393 9085bc96-f90c-0410-a38d-ac8cfabbdae3

--- WebgraF/input.js
+++ WebgraF/input.js
@@ -1,10 +1,6 @@
 info='info.html' 
-mname=['EXAMPLES','monitor'] 
+mname=['EXAMPLES'] 
  v[0]=['ECMWF','RAOBCORE'] 
 d[0]=['',''] 
 info_list[0]=['https://hirlam.org/trac/browser/trunk/harmonie/util/monitor/WebgraF/README'] 
 t[0]=v[0] 
-v[1]=['Surface','Surface_map','Surface_scat','Surface_sign','Surface_skill'] 
-d[1]=['','','','',''] 
- 
-t[1]=v[1] 
